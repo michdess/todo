a2dbdccd344d2e52361135168944184975679cf8
michdess <michaeldessoulavy@gmail.com>
2019-10-19 18:05:27 +1300

    time is set as a var rather than on the fly

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Todo
 [![Build Status](https://travis-ci.org/michdess/todo.svg?branch=master)](https://travis-ci.org/michdess/todo)
 [![StyleCI](https://github.styleci.io/repos/216146156/shield?branch=master)](https://github.styleci.io/repos/216146156)
+[![Code Quality](https://scrutinizer-ci.com/g/michdess/todo/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/michdess/todo/?branch=master)
+
 Create a simple list of tasks to do.
--- tests/Feature/CreateTodoTest.php
+++ tests/Feature/CreateTodoTest.php
@@ -16,15 +16,16 @@ class CreateTodoTest extends TestCase
     {
         $this->signIn();
         $this->withoutExceptionHandling();
+        $due = Carbon::now();
         $response = $this->post('todo', [
             'body' => 'Some task to be done',
-            'due' => Carbon::now(),
+            'due' => $due,
         ]);
         $response->assertStatus(201);
         $this->assertCount(1, Todo::all());
         $this->assertDatabaseHas('todos', [
             'body' => 'Some task to be done',
-            'due' => Carbon::now(),
+            'due' => $due,
         ]);
     }
 
@@ -32,15 +33,16 @@ class CreateTodoTest extends TestCase
     public function an_unauthorised_user_cannot_add_a_new_todo()
     {
         $this->withExceptionHandling();
+        $due = Carbon::now();
         $response = $this->post('todo', [
             'body' => 'Some task to be done',
-            'due' => Carbon::now(),
+            'due' => $due,
         ]);
         $response->assertStatus(302);
         $this->assertCount(0, Todo::all());
         $this->assertDatabaseMissing('todos', [
             'body' => 'Some task to be done',
-            'due' => Carbon::now(),
+            'due' => $due,
         ]);
     }
 }
