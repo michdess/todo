5fd6966138f34f73ca532168c74948e7ec53ca79
michdess <michaeldessoulavy@gmail.com>
2019-10-20 22:25:18 +1300

    basic welcome styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,51 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <title>Todo</title>
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+                background-image: -webkit-linear-gradient(top left, #FEC163 10%, #DE4313 100%);
+                background-image: -o-linear-gradient(top left, #FEC163 10%, #DE4313 100%);
+                background-image: linear-gradient(to bottom right, #FEC163 10%, #DE4313 100%);
+            }   
         </style>
+    <!-- Styles -->
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body class="h-screen brand-color flex items-center justify-center">
+        <div class="max-w-xs w-full overflow-hidden bg-white shadow-lg -mt-32 z-10">
+            <div class="py-6 px-6">
+              <h2 class="text-left text-lg font-normal mb-2 text-gray-800">Welcome back!</h2>
+              <div class="text-sm">
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                @csrf
+                <input id="email" type="email" class="w-full block rounded border px-4 py-2 mb-4 {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email Address" required autofocus>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                <input id="password" type="password" class="w-full block rounded border px-4 py-2 mb-4 {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif        
+                <div class="flex justify-between mb-4">
+                  <label class="text-sm text-gray-600">
+                    <input class="mr-1" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    Remember me
+                  </label>
+                  <a href="{{ route('password.request') }}" class="ml-1 text-sm no-underline font-normal text-gray-800 hover:underline">Forgot password?</a>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <button type="submit" class="bg-orange-500 block text-white font-normal w-full px-4 py-2 rounded">Sign in</button>
+                </form>
+              </div>
+            </div>
+            <div class="bg-gray-300 text-gray-600 text-sm text-center p-4">
+              No account? <a href="/register" class="no-underline text-gray-800 hover:underline font-normal">Register now!</a>
             </div>
         </div>
     </body>
