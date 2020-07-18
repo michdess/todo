73ce36e9b13c073720197501048f4bc7b7145d95
Michael <michaeldessoulavy@gmail.com>
2020-07-18 13:58:55 +0000

    Apply fixes from StyleCI

--- tests/Feature/CompleteTodoTest.php
+++ tests/Feature/CompleteTodoTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use Carbon\Carbon;
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class CompleteTodoTest extends TestCase
 {
